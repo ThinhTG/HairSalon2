e6b355f8fd7ac9c7922802808566c40ef9d0f6de
Truong Nguyen Ngoc Dat <dattnnse173524@fpt.edu.vn>
2024-10-10 16:36:24 +0700

    fix

---
+++ HairSalon/CustomerPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HairSalon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class CustomerPage : Window
+    {
+        public CustomerPage()
+        {
+            InitializeComponent();
+        }
+
+        private void sidebar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = sidebar.SelectedItem as NavButton;
+
+            navframe.Navigate(selected.Navlink);
+        }
+    }
+}
---
+++ HairSalon/StaffPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HairSalon
+{
+    /// <summary>
+    /// Interaction logic for StaffPage.xaml
+    /// </summary>
+    public partial class StaffPage : Window
+    {
+        public StaffPage()
+        {
+            InitializeComponent();
+        }
+
+        private void sidebar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = sidebar.SelectedItem as NavButton;
+
+            navframe.Navigate(selected.Navlink);
+        }
+    }
+}
